2e4f40bff1687683d2278f475e25f571b739eae7
Jaclyn Burge <jacburge@gmail.com>
2016-04-16 16:50:16 -0700

    NEW TEMPLATE, merged content

---
+++ app/static/js/custom.js
@@ -0,0 +1,9 @@
+// Read More
+
+$(document).ready(function(){
+	$('#bio_btn').toggle(function(){
+		$('#bio').css({'display:block'});
+	}, function() {
+		$('#bio').css({'display:none'});
+	},
+	});
