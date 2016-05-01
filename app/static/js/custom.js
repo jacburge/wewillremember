7e29a2f141852d68a65273b3a60c704eb02cead5
Jaclyn Burge <jacburge@gmail.com>
2016-04-30 18:15:45 -0700

    New organization, font, added content and toggle

--- app/static/js/custom.js
+++ app/static/js/custom.js
@@ -1,9 +1,31 @@
 // Read More
 
+// $(document).ready(function(){
+// 	$('#bio_btn').toggle(function(){
+// 		$('#bio').css({'display:block'});
+// 	}, function() {
+// 		$('#bio').css({'display:none'});
+// 	},
+// 	});
+
 $(document).ready(function(){
-	$('#bio_btn').toggle(function(){
-		$('#bio').css({'display:block'});
-	}, function() {
-		$('#bio').css({'display:none'});
-	},
+	$('#hist_btn').click(function(){
+		$('#history').toggle("slow");
+		if ($(this).text() == "Read More") {
+			$(this).text("Read Less");
+		}
+		else {
+			$(this).text('Read More');
+		}
+	});
+	$('#reviews_btn').click(function(){
+		$('#reviews').toggle("slow");
+		if ($(this).text() == "Read More") {
+			$(this).text("Read Less");
+		}
+		else {
+			$(this).text('Read More');
+		}
+	});
+
 	});
